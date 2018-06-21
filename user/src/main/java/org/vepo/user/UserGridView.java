37f9beaaca7936847a94425bb1feabe04905f6b6
Victor Osório <vosorio@v2com.mobi>
2018-06-21 09:51:27 -0300

    User Grid

---
+++ user/src/main/java/org/vepo/user/UserGridView.java
@@ -0,0 +1,31 @@
+package org.vepo.user;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+// import org.eclipse.microprofile.rest.client.RestClientBuilder;
+
+import com.vaadin.data.provider.DataProvider;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Label;
+
+public class UserGridView extends CssLayout {
+	public static final String USER_GRID_VIEW = "user";
+
+	public UserGridView() {
+		// try {
+		// 	URL apiUri = new URL("https://api.randomuser.me/");
+		// 	UserService userService = RestClientBuilder.newBuilder().baseUrl(apiUri).build(UserService.class);
+		// 	Grid<User> grid = new Grid<>(DataProvider.fromCallbacks(query -> userService.list().stream(),
+		// 			query -> userService.list().size()));
+		// 	grid.addColumn(u -> u.getId().getValue());
+		// 	grid.addColumn(u -> u.getEmail());
+		// 	grid.addColumn(u -> u.getName().getFirst());
+		// 	this.addComponent(grid);
+		// } catch (MalformedURLException e) {
+		// 	e.printStackTrace();
+		// }
+		this.addComponent(new Label("User Grid HERE!!!!"));
+	}
+}
--- user/src/main/java/org/vepo/user/UserModule.java
+++ user/src/main/java/org/vepo/user/UserModule.java
@@ -3,13 +3,14 @@ package org.vepo.user;
 import com.vaadin.ui.Notification;
 import org.vepo.api.AppModule;
 import org.vepo.api.ApplicationLayout;
+import org.vepo.api.ApplicationLayout.WorkingAreaComponent;
 
 public class UserModule implements AppModule {
 
     @Override
     public void register(ApplicationLayout layout) {
         ApplicationLayout.MenuOption menuOption = new ApplicationLayout.MenuOption("User");
-        layout.addMenuOption(menuOption, this::optionClicked);
+        layout.addMenuOption(menuOption, (_menuOption) -> layout.addWorkingAreaComponent(new WorkingAreaComponent("User", new UserGridView())));
     }
 
     private void optionClicked(ApplicationLayout.MenuOption menuOption) {
